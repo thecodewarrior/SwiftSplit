c1e3627d7fbf13635d4f11e801995265cd074745
Pierce Corcoran <code@thecodewarrior.dev>
2020-11-25 19:48:32 -0800

    Initial Commit

---
+++ SwiftSplit/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  SwiftSplit
+//
+//  Created by Pierce Corcoran on 11/25/20.
+//  Copyright © 2020 Pierce Corcoran. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
